96f4e81d12ed7102a4b6093dfaef113480646bff
wyom1ng <edhimmel@gmx.net>
2019-11-17 16:09:00 +0100

    callbacks

--- callback/callback.js
+++ callback/callback.js
@@ -1,57 +1,27 @@
-class Fibonacci {
-    #bytes = new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 134, 128, 128, 128, 0, 1, 96, 1, 127, 1, 127, 3, 130, 128, 128, 128, 0, 1, 0, 4, 132, 128, 128, 128, 0, 1, 112, 0, 0, 5, 131, 128, 128, 128, 0, 1, 0, 1, 6, 129, 128, 128, 128, 0, 0, 7, 144, 128, 128, 128, 0, 2, 6, 109, 101, 109, 111, 114, 121, 2, 0, 3, 102, 105, 98, 0, 0, 10, 164, 128, 128, 128, 0, 1, 158, 128, 128, 128, 0, 0, 2, 64, 32, 0, 65, 2, 78, 13, 0, 32, 0, 15, 11, 32, 0, 65, 127, 106, 16, 0, 32, 0, 65, 126, 106, 16, 0, 106, 11]);
-    #term = new Terminal({
-        cols: 200,
-        disableStdin: true,
-        cursorBlink: true,
-    });
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function callbackFunc() {
+    document.body.style.backgroundColor = '#' + randomIntFromInterval(0, 0xFFFFFF).toString(16);
+}
+
+class Callback {
+    #bytes = new Uint8Array([0,97,115,109,1,0,0,0,1,137,128,128,128,0,2,96,1,127,0,96,0,1,127,2,148,128,128,128,0,1,3,101,110,118,12,99,97,108,108,98,97,99,107,70,117,110,99,0,0,3,130,128,128,128,0,1,1,4,132,128,128,128,0,1,112,0,0,5,131,128,128,128,0,1,0,1,6,129,128,128,128,0,0,7,153,128,128,128,0,2,6,109,101,109,111,114,121,2,0,12,99,97,108,108,98,97,99,107,84,101,115,116,0,1,10,144,128,128,128,0,1,138,128,128,128,0,1,1,127,32,0,16,0,32,0,11]);
 
     async init() {
-        const results = await WebAssembly.instantiate(this.#bytes, {imports: {}});
-        this.fibWASM = results.instance.exports.fib;
-
-        this.#term.open(document.getElementById('terminal'));
-    }
-
-    fibJS(n) {
-        if (n <= 1)
-            return n;
-        return this.fibJS(n - 1) + this.fibJS(n - 2);
-    }
-
-    benchmark(iterations) {
-        const jsRes = [];
-        const wasmRes = [];
-
-        const jsStart = performance.now();
-        for (let i = 0; i < iterations; i++) {
-            jsRes.push(this.fibJS(i));
-        }
-        const jsStop = performance.now();
-        const jsDuration = jsStop - jsStart;
-
-        const wasmStart = performance.now();
-        for (let i = 0; i < iterations; i++) {
-            wasmRes.push(this.fibWASM(i));
-        }
-        const wasmStop = performance.now();
-        const wasmDuration = wasmStop - wasmStart;
-
-        this.#term.writeln('JS:\t\t' + jsDuration);
-        this.#term.writeln('WASM:\t\t' + wasmDuration);
-
-        const increase = Math.round(jsDuration / wasmDuration * 100) - 100;
-        this.#term.writeln('Increase:\t' + increase + '%');
-
-        this.#term.writeln('');
-        this.#term.writeln('JS:\t\t[' + jsRes.join(', ') + ']');
-        this.#term.writeln('WASM:\t\t[' + wasmRes.join(', ') + ']');
+        const results = await WebAssembly.instantiate(this.#bytes, {
+            env: {
+                callbackFunc,
+            }
+        });
+        this.callbackTest = results.instance.exports.callbackTest;
     }
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const fib = new Fibonacci();
-    await fib.init();
+    const cb = new Callback();
+    await cb.init();
 
-    fib.benchmark(30);
+    document.onclick = cb.callbackTest;
 });
